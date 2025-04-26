a14e63f48390c0b3d4bead0512f8cdf10199f3d8
NiceTester3000 <vitaliyshopov18@gmail.com>
2025-04-26 15:52:51 +0200

    Fix Mini App URL and switch to node-telegram-bot-api

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const botToken = "7591200004:AAGQN8C0P8SsaaDrWZLqXfSiLe4WJ75rhAI";
 const adminChatId = "5848581114";
 
-const miniAppUrl = "https://inquisitive-platypus-5ea83.netlify.app";
+const miniAppUrl = "https://inquisitive-platypus-5eaa83.netlify.app";
 
 const photoStorage = {};
 
